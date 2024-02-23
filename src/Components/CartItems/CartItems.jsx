87d634b11f2ff8384905a164b1e273cbb9d379f7
Muchammad Ramadhan <muchammadramadhan12@gmail.com>
2024-02-23 17:52:44 +0700

    pembuatan responsive tablet

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,14 +1,13 @@
 import { useContext } from "react";
 import { ShopContext } from "../../Context/ShopContext";
-import RemoveIcon from "../Assets/cart_cross_icon.png";
 import { Link } from "react-router-dom";
 import { Scrollbar } from "../../helper/Scrollbar";
 
 export default function CartItems() {
-  const { all_product, removeCart, cartItems, toggleCheckbox, selectedItems } = useContext(ShopContext);
+  const { all_product, /* removeCart, */ cartItems, toggleCheckbox, selectedItems } = useContext(ShopContext);/* 
   const handleRemoveFromCart = (itemId, size) => {
     removeCart(itemId, size);
-  };
+  }; */
 
   const handleCheck = (itemId, size) => {
     toggleCheckbox(itemId, size);
@@ -26,7 +25,7 @@ export default function CartItems() {
               <th>Price</th>
               <th>Quantity</th>
               <th>Size</th>
-              <th>Remove</th>
+              <th>Edit</th>
               <th>Total</th>
             </tr>
           </thead>
@@ -39,7 +38,7 @@ export default function CartItems() {
                 return (
                   <tr key={product.id}>
                     <td>
-                      <input type="checkbox" checked={isChecked} onChange={() => handleCheck(product.id, sizes)} />
+                      <input className="checkbox" type="checkbox" checked={isChecked} onChange={() => handleCheck(product.id, sizes)} />
                     </td>
                     <td>
                       <img src={product.image} alt={product.name} className="carticon-product-icon" />
@@ -49,7 +48,10 @@ export default function CartItems() {
                     <td>{item.quantity}</td>
                     <td>{sizes}</td>
                     <td>
-                      <img src={RemoveIcon} alt="" onClick={() => handleRemoveFromCart(product.id, item.sizes[0])} className="cartItems-action" />
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"className="edit" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                      </svg>
                     </td>
                     <td>${product.new_price * item.quantity}</td>
                   </tr>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./Main.css";
+import "./Responsive.css";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Router.jsx";
 import ShopContextProvider from "./Context/ShopContext.jsx";
